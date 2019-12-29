2d2968ed5bb67b7f447650d38f3406b4abbd3928
andymememe <andymememe@gmail.com>
2019-12-29 15:31:58 +0800

    Fix: Remove Opal for deadlink & Change three.js directory structure

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use("/css", express.static(__dirname + '/css'));
 app.use("/res", express.static(__dirname + '/res'));
 
 /* Local JS Requirement */
-app.use("/threejs/js", express.static(__dirname + '/node_modules/three.js/build'));
+app.use("/threejs/js", express.static(__dirname + '/node_modules/three'));
 
 app.listen(3000);
